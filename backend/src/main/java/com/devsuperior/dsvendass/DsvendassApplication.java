d0cbbdac14f1d548b9efc73b080c2c9a48e7aa6a
Alyson Gadelha <alyson_sud@hotmail.com>
2021-09-06 19:58:44 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsvendass/DsvendassApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvendass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendassApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsvendass/DsvendassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvendass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
